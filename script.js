201333d44d4c26ad71d1198bd006495b3f8b704f
Meedux <herrerojayemark@gmail.com>
2024-12-14 12:58:00 +0800

    Updated to use motion instead

--- script.js
+++ script.js
@@ -4,47 +4,29 @@ window.addEventListener("load", () => {
     document.getElementById("main-content").style.display = "block";
     // document.body.style.overflow = "auto"; // Make the body scrollable again
   }, 4000); // Adjust the delay as needed
+
+  const { animate, scroll } = Motion;
+  scroll((progress) => {
+    animate(
+      "#top-path",
+      {pathLength: [0, 1]},
+      {duration: 5}
+    )
+  }, { 
+    target: document.getElementById("top-svg"),
+  })
+
+  scroll((progress) => {
+    animate(
+      "#svgPath",
+      {pathLength: [0, 1]},
+      {duration: 10}
+    )
+  }, { 
+    target: document.getElementById("scrollingSvg"),
+    // offset: "start start"
+  })
 });
 
-// Register GSAP plugins
-gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
 
-// Get the SVG path element
-const svgPath = document.querySelector("#svgPath");
-const svg = document.querySelector("#scrollingSvg");
 
-const orgSvg = document.querySelector("#top-img");
-
-// Set up the path animation (stroke-dashoffset)
-const pathLength = svgPath.getTotalLength();
-svgPath.style.strokeDasharray = pathLength;
-svgPath.style.strokeDashoffset = pathLength;
-
-gsap.to(svgPath, {
-  strokeDashoffset: 0, // Animate the stroke to reveal the path
-  ease: "power2.out",
-  scrollTrigger: {
-    trigger: svgPath,
-    start: "top 50%",
-    end: "bottom 10%",
-    scrub: 0.5,
-    // markers: true,
-  },
-});
-
-// Set up motion path animation for the SVG
-gsap.to(svg, {
-  scrollTrigger: {
-    trigger: svgPath,
-    start: "top 50%",
-    end: "bottom 50%",
-    scrub: 0.5,
-    // markers: true,
-  },
-  motionPath: {
-    path: svgPath,
-    align: svgPath,
-    autoRotate: true,
-    alignOrigin: [0.5, 0.5],
-  },
-});
